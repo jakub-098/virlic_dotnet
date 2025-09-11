29f0f815359028613aec5b10333283ddd4eab733
jakubvirlic <virlicjakub@gmail.com>
2025-09-11 08:43:21 +0200

    uloha 2

---
+++ a/Controllers/UlohyController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace a.Controllers
+{
+
+    public class ulohyController : Controller
+    {
+        public IActionResult uloha1()
+        {
+            return View();
+        }
+        public IActionResult uloha2()
+        {
+            return View();
+        }
+    }
+}
