6dd51da837a8efc8faeb14ca78762fa74e7a0f5b
jakubvirlic <virlicjakub@gmail.com>
2025-09-25 10:41:36 +0200

    nove testy

--- a/obj/Debug/net9.0/a.AssemblyInfo.cs
+++ a/obj/Debug/net9.0/a.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("a")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29f0f815359028613aec5b10333283ddd4eab733")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdf477607ffa2003dcbf07294dd6f0d7db8f383f")]
 [assembly: System.Reflection.AssemblyProductAttribute("a")]
 [assembly: System.Reflection.AssemblyTitleAttribute("a")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
